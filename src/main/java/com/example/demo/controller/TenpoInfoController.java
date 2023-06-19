b1fc68e78caec1e56609fd7d930d61b79bb6badc
kurenisu <a84hg61zn57ip@gmail.com>
2023-06-19 14:52:16 +0900

    controllerとserviceクラスの更新

--- src/main/java/com/example/demo/controller/TenpoInfoController.java
+++ src/main/java/com/example/demo/controller/TenpoInfoController.java
@@ -1,8 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import dto.TenpoAddRequest;
+import dto.TenpoSearchRequest;
+import dto.TenpoUpdateRequest;
+import entity.TenpoInfo;
 import service.TenpoInfoService;
 
 /**
@@ -17,6 +33,116 @@ public class TenpoInfoController {
 	@Autowired
 	private TenpoInfoService tenpoInfoService;
 	
+    /**
+     * 店舗情報一覧画面を表示
+     * @param model Model
+     * @return 店舗情報一覧画面
+     */
+    @GetMapping(value = "/tenpoinfo/list")
+    public String displayList(Model model) {
+        List<TenpoInfo> tenpoList = tenpoInfoService.findAll();
+        model.addAttribute("tenpolist", tenpoList);
+        model.addAttribute("tenpoSearchRequest", new TenpoSearchRequest());
+        return "tenpoinfo/search";
+    }
 	
+    /**
+     * 店舗新規登録画面を表示
+     * @param model Model
+     * @return 店舗情報一覧画面
+     */
+    @GetMapping(value = "/tenpoinfo/add")
+    public String displayAdd(Model model) {
+        model.addAttribute("tenpoAddRequest", new TenpoAddRequest());
+        return "tenpoinfo/add";
+    }
+    
+    /**
+     * 店舗編集画面を表示
+     * @param id ID
+     * @param model Model
+     * @return 店舗編集画面
+     */
+    @GetMapping("/tenpoinfo/{id}/edit")
+    public String displayEdit(@PathVariable Long id, Model model) {
+        TenpoInfo tenpo = tenpoInfoService.findById(id);
+        TenpoUpdateRequest tenpoUpdateRequest = new TenpoUpdateRequest();
+        tenpoUpdateRequest.setId(tenpo.getId());
+        tenpoUpdateRequest.setId(tenpo.getId());
+        tenpoUpdateRequest.setName(tenpo.getName());
+        tenpoUpdateRequest.setPhone(tenpo.getPhone());
+        tenpoUpdateRequest.setAddress(tenpo.getAddress());
+        model.addAttribute("tenpoUpdateRequest", tenpoUpdateRequest);
+        return "tenpoinfo/edit";
+    }
+    
+    /**
+     * 店舗情報検索
+     * @param tenpoSearchRequest リクエストデータ
+     * @param model Model
+     * @return 店舗情報一覧画面
+     */
+    @RequestMapping(value = "/tenpoinfo/search", method = RequestMethod.POST)
+    public String search(@ModelAttribute TenpoSearchRequest tenpoSearchRequest, Model model) {
+        List<TenpoInfo> tenpoList = tenpoInfoService.search(tenpoSearchRequest);
+        model.addAttribute("tenpolist", tenpoList);
+        return "tenpoinfo/search";
+    }
+    
+    /**
+     * 店舗情報削除（論理削除）
+     * @param id ID
+     * @param model Model
+     * @return 店舗情報一覧画面
+     */
+    @GetMapping("/tenpoinfo/{id}/delete")
+    public String delete(@PathVariable Long id, Model model) {
+        // 店舗情報の削除
+        tenpoInfoService.delete(id);
+        return "redirect:/tenpoinfo/list";
+    }
+    
+    /**
+     * 店舗新規登録
+     * @param tenpoRequest リクエストデータ
+     * @param model Model
+     * @return 店舗情報一覧画面
+     */
+    @RequestMapping(value = "/tenpoinfo/create", method = RequestMethod.POST)
+    public String create(@Validated @ModelAttribute TenpoAddRequest userRequest, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            // 入力チェックエラーの場合
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError error : result.getAllErrors()) {
+                errorList.add(error.getDefaultMessage());
+            }
+            model.addAttribute("validationError", errorList);
+            return "tenpoinfo/add";
+        }
+        // 店舗情報の登録
+        tenpoInfoService.save(userRequest);
+        return "redirect:/tenpoinfo/list";
+    }
 
+    /**
+     * 店舗更新
+     * @param tenpoRequest リクエストデータ
+     * @param model Model
+     * @return 店舗情報詳細画面
+     */
+    @RequestMapping(value = "/tenpoinfo/update", method = RequestMethod.POST)
+    public String update(@Validated @ModelAttribute TenpoUpdateRequest tenpoUpdateRequest, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError error : result.getAllErrors()) {
+                errorList.add(error.getDefaultMessage());
+            }
+            model.addAttribute("validationError", errorList);
+            return "tenpoinfo/edit";
+        }
+        // ユーザー情報の更新
+        tenpoInfoService.update(tenpoUpdateRequest);
+        return "redirect:/tenpoinfo/list";
+    }
+    
 }
--- src/main/java/service/TenpoInfoService.java
+++ src/main/java/service/TenpoInfoService.java
@@ -1,9 +1,15 @@
 package service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dao.TenpoInfoMapper;
+import dto.TenpoAddRequest;
+import dto.TenpoSearchRequest;
+import dto.TenpoUpdateRequest;
+import entity.TenpoInfo;
 
 /**
  * 店舗情報 Service
@@ -17,5 +23,53 @@ public class TenpoInfoService {
      */
     @Autowired
     private TenpoInfoMapper tenpoInfoMapper;
+    
+    /**
+     * 店舗情報全件検索
+     * @return 検索結果
+     */
+    public List<TenpoInfo> findAll() {
+        return tenpoInfoMapper.findAll();
+    }
+    
+    /**
+     * 店舗情報主キー検索
+     * @return 検索結果
+     */
+    public TenpoInfo findById(Long id) {
+        return tenpoInfoMapper.findById(id);
+    }
+    
+    /**
+     * 店舗情報検索
+     * @param tenpoSearchRequest リクエストデータ
+     * @return 検索結果
+     */
+    public List<TenpoInfo> search(TenpoSearchRequest tenpoSearchRequest) {
+        return tenpoInfoMapper.search(tenpoSearchRequest);
+    }
+    
+    /**
+     * 店舗情報登録
+     * @param tenpoAddRequest リクエストデータ
+     */
+    public void save(TenpoAddRequest tenpoAddRequest) {
+        tenpoInfoMapper.save(tenpoAddRequest);
+    }
 
+    /**
+     * 店舗情報更新
+     * @param tenpoEditRequest リクエストデータ
+     */
+    public void update(TenpoUpdateRequest tenpoUpdateRequest) {
+        tenpoInfoMapper.update(tenpoUpdateRequest);
+    }
+    /**
+     * 店舗情報論理削除
+     * @param id
+     */
+    public void delete(Long id) {
+        tenpoInfoMapper.delete(id);
+    }
+    
 }
