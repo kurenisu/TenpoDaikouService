7d212f21587229679be75834e85b32adcaf53223
kurenisu <a84hg61zn57ip@gmail.com>
2023-06-19 13:14:32 +0900

    店舗代行サービスプロジェクト作成

---
+++ src/main/java/com/example/demo/controller/TenpoInfoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 店舗情報 Controller
+ */
+@Controller
+public class TenpoInfoController {
+	
+	/**
+	 * 店舗情報 Service
+	**/
+	private TenpoInfoService tenpoInfoService;
+	
+	
+
+}
